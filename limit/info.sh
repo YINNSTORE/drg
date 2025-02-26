156f416a34036402d1d8ce2ab17458973dabafd0
YINNSTORE <146443900+YINNSTORE@users.noreply.github.com>
2025-02-26 23:35:45 +0700

    Add files via upload

---
+++ limit/info.sh
@@ -0,0 +1,7 @@
+clear
+figlet -f 3d "Yinn Vpn Premium Tunneling" | lolcat
+cat /root/log-install.txt
+echo "-----------------------------------------------------"
+echo "                Credit Script @YinnVpn" | lolcat
+echo "                   Author Script Yinn Vpn Premium" | lolcat
+echo "-----------------------------------------------------"
